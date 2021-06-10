9c220638c2db54ac2852cd1cf9610174f30eeded
Ricardo Lazaro <ricardoalazaro@hotmail.com>
2021-06-09 23:46:19 -0300

    Test for Task of chapter 2 passing

---
+++ bds02/src/main/java/com/devsuperior/bds02/controllers/EventController.java
@@ -0,0 +1,42 @@
+package com.devsuperior.bds02.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+	
+	@Autowired
+	private EventService service;
+
+	@GetMapping
+	public ResponseEntity <List<EventDTO>> findAll(){ 
+		List<EventDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+}
